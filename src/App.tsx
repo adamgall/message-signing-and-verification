a8640a5267feac84a5cf68f44a0bab682b42c48e
Adam Gall <galladamj@gmail.com>
2024-10-11 22:16:09 -0400

    Update title

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import MessageSignVerify from "./MessageSignVerify";
 function App() {
   return (
     <div>
-      <h1>Web3 Wallet Connection and Message Signing</h1>
+      <h1>Message Signing and Verification</h1>
       <w3m-button />
       <MessageSignVerify />
     </div>
